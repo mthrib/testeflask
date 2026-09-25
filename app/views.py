2c755d051b3047e7e4536280d2d93cc36dbfaabd
mthrib <matheuseyorrana@gmail.com>
2026-09-25 11:41:53 +0000

    v1.01

--- app/views.py
+++ app/views.py
@@ -1,2 +1,10 @@
 from app import app
 from flask import render_template , url_for
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from app import app
 
+from app import views
+
 if __name__ == '__main__':
     app.run(debug=True)
