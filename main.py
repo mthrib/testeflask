af7c653808a89e42a4adafd0af2b30ca54a968bf
mthrib <matheuseyorrana@gmail.com>
2026-09-25 11:30:02 +0000

    v1

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from flask import Flask, render_template
+from app import app
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/loginonde")
-def login():
-    return render_template("login.html")
-
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
